f9747e1963eb5fefa8ef2654ebac6ba8251aea7e
matheulavigne <feellowes@hotmail.com>
2014-07-07 08:41:55 -0400

    New branch name for languages website

---
+++ language/fr/site_lang.php
@@ -0,0 +1,68 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Un outils pour développeurs qui vous fera sauver du temps. Il détecte les navigateurs désuets et recommande aux utilisateurs de mettre à jour vers la version la plus récente.';
+$lang['btn_closenav'] = 'Fermer la navigation';
+$lang['mainNav_home'] = 'ACCUEIL';
+$lang['mainNav_project'] = 'LE PROJET';
+$lang['mainNav_how'] = 'FONCTIONNEMENT';
+$lang['mainNav_faq'] = 'FAQ<em>s</em>';
+$lang['mainNav_git'] = 'PAGE GITHUB';
+
+/*SHARE*/
+$lang['share_share'] = 'Partager';
+$lang['share_twitter'] = 'Un %23outils pour %23développeurs qui vous fera sauver du temps. Il détecte les %23navigateurs %23désuets et recommande aux utilisateurs de %23mettre à jour vers la version la plus récente.';
+$lang['share_twitter_t'] = 'Partager sur Twitter';
+$lang['share_fb_t'] = 'Partager sur Facebook';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Fermer';
+$lang['txt_keepcalm'] = 'on se calme <br><span class="xs">ceci est un outils pour</span>ordinateur<br><span class="xs">et non pour</span> appareils mobiles';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Pour une expérience optimale, gardez votre navigateur à jour. Obtenez ici les versions les plus récentes.';
+$lang['home_subtitle_mobile'] = 'Ceci est un outils pour ordinateur, <br>mais libre à vous d&rsquo;explorer.';
+$lang['home_percentage'] = 'des internautes utilisent <br>ce navigateur';
+$lang['home_download'] = 'TÉLÉCHARGER';
+$lang['home_version'] = 'VERSION';
+$lang['home_available'] = 'DISPONIBLE POUR';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Une cause <br> perdue!';
+$lang['proj_desc1'] = 'Après avoir passé beaucoup trop de temps à optimisé nos sites Web pour les anciens navigateurs, nous, chez Büro, avons décidé de créer une solution qui non seulement vous fera sauver du temps, mais vous permettra de rentrer à la maison plus tôt.<br><br> La pizza n&rsquo;est pas incluse!';
+$lang['proj_alt1'] = 'désespéré';
+$lang['proj_desc1_2'] = 'Un développeur en perte de contrôle';
+
+$lang['proj_title2'] = 'Comment <br>ça fonctionne?';
+$lang['proj_desc2'] = 'Lorsque mis en oeuvre, ce script détecte si le navigateur supporte une propriété CSS 3. Si la propriété n&rsquo;est pas supportée, nous afficherons un message d&rsquo;alerte visuellement intéressant recommandant à l&rsquo;utilisateur de mettre à jour son navigateur.';
+$lang['proj_desc2_2'] = 'Un site Web s&rsquo;affichant sur un ancien navigateur';
+
+$lang['proj_title3'] = '3 étapes pour <br> commencer à l&rsquo;utiliser!';
+$lang['proj_desc3'] = 'Ça ne prend que 3 minutes. Ajouter les fichiers CSS et Javascript et coller quelques lignes de code HTML. <br><br>Pour plus d&rsquo;informations, visitez la page <a class="github" href="https://github.com/burocratik/outdated-browser">Github</a> ;)';
+$lang['proj_desc3_2'] = 'Conçu chez <br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Télécharger <br> le projet';
+$lang['how_desc1'] = 'Notre projet contient tous les fichiers nécessaires: CSS et Javascript. Nous avons même ajouté un exemple avec le code HTML pour le message d&rsquo;alerte.';
+$lang['how_desc1_2'] = 'Télécharger sur Github';
+
+$lang['how_title2'] = 'Inclure à <br> votre projet';
+$lang['how_desc2'] = 'Ajouter les fichiers CSS dans l&rsquo;élément &#x3c;head&#x3e; et les fichiers Javascript au bas de l&rsquo;élément &#x3c;body&#x3e;.';
+$lang['how_desc2_2'] = 'Copier le code dans la presse-papier ';
+$lang['how_alt2'] = 'navigteur';
+
+$lang['how_title3'] = 'Ajouter le <br> code HTML <br> nécessaire';
+$lang['how_desc3'] = 'Vous pouvez placer ce code immédiatement avant la balise &#x3c;/body&#x3e;.';
+$lang['how_desc3_2'] = '
+&#x3c;h6&#x3e;Votre navigateur est désuet!&#x3c;/h6&#x3e;
+&#x3c;p&#x3e;Mettez à jour votre navigateur pour afficher correctement ce site Web. &#x3c;a id="btnUpdateBrowser" href="http://outdatedbrowser.com/"&#x3e;Mettre à jour maintenant&#x3c;/a&#x3e;&#x3c;/p&#x3e;
+&#x3c;p class="last"&#x3e; &#x3c;a href="#" id="btnCloseUpdateBrowser" title="Fermer"&#x3e; &times; &#x3c;/a&#x3e; &#x3c;/p&#x3e;
+';
+
+$lang['how_title4'] = 'Appellez <br> l&rsquo;extension et';
+$lang['how_alt4'] = 'réussi';
+$lang['how_desc4'] = 'À partir de maintenant, tout utilisateur avec un navigateur désuet recevra un avertissement distigué dans le haut de la page.<br><br> Si vous aimez ce projet, <i>forkez</i> le et partagez le avec le monde entier!';
+$lang['how_desc4_2'] = '//méthode d\'initialisation, placez à l\'intérieur d\'une fonction DOM ready';
+
