057664d46d3851780798abb3611173070fe2c952
Vaclav Pavek <developer@vaclavpavek.cz>
2014-07-08 21:49:44 +0200

    add Czech translation

---
+++ language/cs/site_lang.php
@@ -0,0 +1,67 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Nástroj pro vývojáře šetřící čas. Detekuje zastaralé prohlížeče a radí uživatelům, aby přešli na novou verzi.';
+$lang['btn_closenav'] = 'Zavřít navigaci';
+$lang['mainNav_home'] = 'ÚVODNÍ STRÁNKA';
+$lang['mainNav_project'] = 'O PROJEKTU';
+$lang['mainNav_how'] = 'JAK POUŽÍVAT';
+$lang['mainNav_faq'] = 'NEJČASTĚJŠÍ OTÁZKY';
+$lang['mainNav_git'] = 'GITHUB';
+
+/*SHARE*/
+$lang['share_share'] = 'Sdílet';
+$lang['share_twitter'] = '%23Nástroj pro %23vývojáře šetřící čas. Detekuje #zastaralé #prohlížeče a radí uživatelům, aby přešli na novou verzi.';
+$lang['share_twitter_t'] = 'Sdílet na Twitter';
+$lang['share_fb_t'] = 'Sdílet na Facebook';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Zavřít';
+$lang['txt_keepcalm'] = 'buďte v klidu <br><span class="xs">toto je nástroj</span>desktop<br><span class="xs">ne</span> mobilní zařízení';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Pro lepší zážitek udržujte váš prohlížeč aktuální. Zkontrolujte zda máte nejnovější verze.';
+$lang['home_subtitle_mobile'] = 'Toto je nástroj pro desktop, <br>ale klidně si jej prozkoumejte.';
+$lang['home_percentage'] = 'lidé používající <br>tento prohlížeč';
+$lang['home_download'] = 'STÁHNOUT';
+$lang['home_version'] = 'VERZE';
+$lang['home_available'] = 'DOSTUPNÝ PRO';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Příčina <br> zoufalství!';
+$lang['proj_desc1'] = 'Poté, co jste strávili příliš mnoho času optimalizací vaší webové stránky pro staré prohlížeče, jsme na Büro rozhodli vytvořit řešení, které nejen šetří váš čas, ale také vás domu pošle dříve. <br> Pizza není součástí dodávky!';
+$lang['proj_alt1'] = 'příčina';
+$lang['proj_desc1_2'] = 'Vývojáři šílí';
+
+$lang['proj_title2'] = 'Jak to <br>funguje?';
+$lang['proj_desc2'] = 'Po spuštění tohoto skriptu zjistí, zda prohlížeč podporuje CSS3 vlastnosti. Pokud tomu tak není, ukáže se pěkný a cool vypadající upozornění pro uživatele aby aktualizoval prohlížeč.';
+$lang['proj_desc2_2'] = 'Web se zobrazuje ve neaktuápním prohlížeči';
+
+$lang['proj_title3'] = '3 kroky k <br> používání';
+$lang['proj_desc3'] = 'Trvá to jen 3 minuty zahrnout JS a CSS soubor a vložit pár řádků HTML. <br> Pro více informací zkontrolujte stránku na <a class="github" href="https://github.com/burocratik/outdated-browser"> Github </ a> ;)';
+$lang['proj_desc3_2'] = 'Vytvořeno v <br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Stáhnout <br> projekt';
+$lang['how_desc1'] = 'Náš balíček obsahuje všechny potřebné soubory JS a CSS. Přidali jsme také příklad se strukturou HTML pro zprávy.';
+$lang['how_desc1_2'] = 'Stáhnout z Github';
+
+$lang['how_title2'] = 'Vložte do <br> vašeho projektu';
+$lang['how_desc2'] = 'Vložte css soubor do &#x3c;head&#x3e; elementu a JS soubor nakonec &#x3c;body&#x3e;.';
+$lang['how_desc2_2'] = 'Zkopírovat kód do schránky';
+$lang['how_alt2'] = 'prohlížeč';
+
+$lang['how_title3'] = 'Vložte do <br> požadovaného <br> HTML';
+$lang['how_desc3'] = 'Můžete vložit před &#x3c;/body&#x3e; tag.';
+$lang['how_desc3_2'] = '
+&#x3c;h6&#x3e;Váš prohlížeč je zastaralý!&#x3c;/h6&#x3e;
+&#x3c;p&#x3e;Pro správné zobrazení aktualizujte váš prohlížeč. &#x3c;a id="btnUpdateBrowser" href="http://outdatedbrowser.com/"&#x3e;Aktualizovat nyní svůj prohlížeč&#x3c;/a&#x3e;&#x3c;/p&#x3e;
+&#x3c;p class="last"&#x3e; &#x3c;a href="#" id="btnCloseUpdateBrowser" title="Zavřít"&#x3e; &times; &#x3c;/a&#x3e; &#x3c;/p&#x3e;
+';
+
+$lang['how_title4'] = 'Spusťte <br> plugin a';
+$lang['how_alt4'] = 'dobře';
+$lang['how_desc4'] = 'Od této chvíle bude každý uživatel se starým prohlížečem upozorněn efektním oznámením v horní části stránky. <br><br> Pokud se vám líbí tento projekt, forkujte a sdílejte s celým světem!';
+$lang['how_desc4_2'] = '//plugin funkce místo uvnitř DOM ready funkce';
