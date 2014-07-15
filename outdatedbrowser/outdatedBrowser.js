27c7e9c6da0aec7c66c7832dc153c0cdfa90a10c
Alexandre PRG <alex@burocratik.com>
2014-07-15 11:21:38 +0100

    AJAX tests - not working, do not use this (test only!)

--- outdatedbrowser/outdatedBrowser.js
+++ outdatedbrowser/outdatedBrowser.js
@@ -138,4 +138,58 @@ var outdatedBrowser = function(options) {
         this.style.color = txtColor;
         this.style.backgroundColor = bkgColor;
     };
-}//end of function
+
+    //NEW LANG
+
+
+
+
+}//end of outdatedBrowser function
+
+
+    //Bulletproof Ajax by Jeremy Keith
+    function getHTTPObject() {
+      var xhr = false;
+      if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+      } else if (window.ActiveXObject) {
+        try {
+          xhr = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch(e) {
+          try {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+          } catch(e) {
+            xhr = false;
+          }
+        }
+      }
+      return xhr;
+    };//end function
+
+    function grabFile(file) {
+      var request = getHTTPObject();
+      if (request) {
+        request.onreadystatechange = function() {
+          displayResponse(request);
+        };
+        request.open("GET", file, true);
+        request.send(null);
+      }
+      return false;
+    };//end grabFile
+
+    function displayResponse(request) {
+      if (request.readyState == 4) {
+        if (request.status == 200 || request.status == 304) {
+          alert(request.responseText);
+        }
+      }
+      return false;
+    };//end displayResponse
+
+
+var btnTESTE = document.getElementById("alex");
+btnTESTE.onclick=function(){
+    grabFile("pt.txt");
+    return false;
+};
