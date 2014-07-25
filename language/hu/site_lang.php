05d9b9abeb945f82f3eed81e8b138c40f43fc51a
blackfyre <gnick666@gmail.com>
2014-07-25 14:40:39 +0200

    begun translation

---
+++ language/hu/site_lang.php
@@ -0,0 +1,68 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Egy idő megtakarító eszköz fejlesztők számára. Azonosítja a régi böngészőket és javasoloja a frissítést.';
+$lang['btn_closenav'] = 'Bezár';
+$lang['mainNav_home'] = 'NYITÓLAP';
+$lang['mainNav_project'] = 'A PROJEKT';
+$lang['mainNav_how'] = 'HOGYAN';
+$lang['mainNav_faq'] = 'GYIK<em>ok</em>';
+$lang['mainNav_git'] = 'GITHUB OLDAL';
+
+/*SHARE*/
+$lang['share_share'] = 'Megosztás';
+$lang['share_twitter'] = 'Egy idő megtakarító eszköz fejlesztők számára. Azonosítja a régi böngészőket és javasoloja a frissítést.';
+$lang['share_twitter_t'] = 'Megosztás Twitteren';
+$lang['share_fb_t'] = 'Megosztás Facebookon';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Bezár';
+$lang['txt_keepcalm'] = 'NYUGI <br><span class="xs">ez az eszköz</span>asztali gépekre<br><span class="xs">és nem</span> mobil eszközökre készült';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'A jobb élményért tartsa naprakészen a böngészőjét. Itt találhatóak a legfrissebbek.';
+$lang['home_subtitle_mobile'] = 'Ez az eszköz asztali gépekre készült, <br>de fedezd fel nyugodtan.';
+$lang['home_percentage'] = 'ember használja <br>ezt a böngészőt';
+$lang['home_download'] = 'LETÖLTÉS';
+$lang['home_version'] = 'VERZIÓ';
+$lang['home_available'] = 'ELÉRHETŐ';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Egy Kétségbeesett<br> Ügy!';
+$lang['proj_desc1'] = 'Miután túl sok időt töltöttünk azzal, hogy elavult böngészőkre is elérhetővé tegyük az oldalainkat, mi a Büro-nál úgy döntöttünk, hogy létrehozunk egy megoldást ami nem csak időt takarít meg, de korábban is hazajutunk.<br><br> Pizzát nem mellékelünk!';
+$lang['proj_alt1'] = 'kétségbeesett';
+$lang['proj_desc1_2'] = 'Egy őrült fejlesztő';
+
+$lang['proj_title2'] = 'Hogyan <br>működik?';
+$lang['proj_desc2'] = 'When executed, this script detects whether the browser supports a CSS3 property. If it doesn&rsquo;t, we&rsquo;ll show a nice and cool looking warning advising the user to update the browser.';
+$lang['proj_desc2_2'] = 'A website running on an old browser';
+
+$lang['proj_title3'] = '3 Steps to <br> Start Using It!';
+$lang['proj_desc3'] = 'It only takes 3 minutes. Include the JS and CSS file, and paste a couple of HTML lines. <br><br>For more info check the <a class="github" href="https://github.com/burocratik/outdated-browser">Github</a> page ;)';
+$lang['proj_desc3_2'] = 'Developed at <br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Download <br> the Package';
+$lang['how_desc1'] = 'Our package contains all the required files: JS and CSS. We&rsquo;ve also added an example with the HTML structure for the message.';
+$lang['how_desc1_2'] = 'Download on Github';
+
+$lang['how_title2'] = 'Include them <br> in Your Project';
+$lang['how_desc2'] = 'Load the css file in the &#x3c;head&#x3e; element and the JS file at the end of the &#x3c;body&#x3e;.';
+$lang['how_desc2_2'] = 'Copy this code to clipboard ';
+$lang['how_alt2'] = 'browser ';
+
+$lang['how_title3'] = 'Include the <br> Required <br> HTML';
+$lang['how_desc3'] = 'You can place this code right before the &#x3c;/body&#x3e; tag.';
+$lang['how_desc3_2'] = '
+&#x3c;h6&#x3e;Your browser is out-of-date!&#x3c;/h6&#x3e;
+&#x3c;p&#x3e;Update your browser to view this website correctly. &#x3c;a id="btnUpdateBrowser" href="http://outdatedbrowser.com/"&#x3e;Update my browser now&#x3c;/a&#x3e;&#x3c;/p&#x3e;
+&#x3c;p class="last"&#x3e; &#x3c;a href="#" id="btnCloseUpdateBrowser" title="Close"&#x3e; &times; &#x3c;/a&#x3e; &#x3c;/p&#x3e;
+';
+
+$lang['how_title4'] = 'Call the <br> Plugin and';
+$lang['how_alt4'] = 'done';
+$lang['how_desc4'] = 'From now on, any user with an old browser will be presented with a fancy notice on the top of the page.<br><br> If you like this project fork it and share it with the world!';
+$lang['how_desc4_2'] = '//plugin function, place inside DOM ready function';
+
