d2256802e9c57cb079a1cb9496840f4e1e754d9c
Meki <gnick666@gmail.com>
2014-07-26 09:19:34 +0200

    Progress update
    unstalbe internet connection...

--- language/hu/site_lang.php
+++ language/hu/site_lang.php
@@ -36,15 +36,15 @@ $lang['proj_alt1'] = 'kétségbeesett';
 $lang['proj_desc1_2'] = 'Egy őrült fejlesztő';
 
 $lang['proj_title2'] = 'Hogyan <br>működik?';
-$lang['proj_desc2'] = 'When executed, this script detects whether the browser supports a CSS3 property. If it doesn&rsquo;t, we&rsquo;ll show a nice and cool looking warning advising the user to update the browser.';
+$lang['proj_desc2'] = 'Futtatáskor a böngésző CSS3 támogatása ellenőrzésre kerül. Ha hiányos a támogatás, megjelenítünk szép és látványos értesítést, hogy nem ártana frissíteni vagy lecserélni a bönészőt.';
 $lang['proj_desc2_2'] = 'A website running on an old browser';
 
-$lang['proj_title3'] = '3 Steps to <br> Start Using It!';
-$lang['proj_desc3'] = 'It only takes 3 minutes. Include the JS and CSS file, and paste a couple of HTML lines. <br><br>For more info check the <a class="github" href="https://github.com/burocratik/outdated-browser">Github</a> page ;)';
+$lang['proj_title3'] = '3 lépés <br> hogy használd!';
+$lang['proj_desc3'] = 'Csak 3 perc a beépítés. Add az oldalhosz a CSS és JS fájlokat, és másolj be egy pár sor HTML kódot. <br><br>A részleteket megtalálod a <a class="github" href="https://github.com/burocratik/outdated-browser">Github</a> oldalunkon';
 $lang['proj_desc3_2'] = 'Developed at <br> Bürolabs';
 
 /*HOW*/
-$lang['how_title1'] = 'Download <br> the Package';
+$lang['how_title1'] = 'A csomag <br> letöltése';
 $lang['how_desc1'] = 'Our package contains all the required files: JS and CSS. We&rsquo;ve also added an example with the HTML structure for the message.';
 $lang['how_desc1_2'] = 'Download on Github';
 
@@ -56,13 +56,13 @@ $lang['how_alt2'] = 'browser ';
 $lang['how_title3'] = 'Include the <br> Required <br> HTML';
 $lang['how_desc3'] = 'You can place this code right before the &#x3c;/body&#x3e; tag.';
 $lang['how_desc3_2'] = '
-&#x3c;h6&#x3e;Your browser is out-of-date!&#x3c;/h6&#x3e;
-&#x3c;p&#x3e;Update your browser to view this website correctly. &#x3c;a id="btnUpdateBrowser" href="http://outdatedbrowser.com/"&#x3e;Update my browser now&#x3c;/a&#x3e;&#x3c;/p&#x3e;
-&#x3c;p class="last"&#x3e; &#x3c;a href="#" id="btnCloseUpdateBrowser" title="Close"&#x3e; &times; &#x3c;/a&#x3e; &#x3c;/p&#x3e;
+ &lt;h6&gt;A b&ouml;ng&eacute;szője elavult!&lt;/h6&gt;
+ &lt;p&gt;Firss&iacute;tse vagy cser&eacute;lje le a b&ouml;ng&eacute;szőj&eacute;t. &lt;a id=&quot;btnUpdateBrowser&quot; href=&quot;http://outdatedbrowser.com/&quot;&gt;A b&ouml;ng&eacute;szpm friss&iacute;t&eacute;se &lt;/a&gt;&lt;/p&gt;
+ &lt;p class=&quot;last&quot;&gt;&lt;a href=&quot;#&quot; id=&quot;btnCloseUpdateBrowser&quot; title=&quot;Close&quot;&gt;&amp;times;&lt;/a&gt;&lt;/p&gt;
 ';
 
 $lang['how_title4'] = 'Call the <br> Plugin and';
-$lang['how_alt4'] = 'done';
+$lang['how_alt4'] = 'kész';
 $lang['how_desc4'] = 'From now on, any user with an old browser will be presented with a fancy notice on the top of the page.<br><br> If you like this project fork it and share it with the world!';
 $lang['how_desc4_2'] = '//plugin function, place inside DOM ready function';
 
