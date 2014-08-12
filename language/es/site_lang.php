e0e293145b5366a45a19ad8757f1fcdfc2773fb7
Alexandre PRG <alex@burocratik.com>
2014-08-12 14:41:23 +0100

    Spanish

---
+++ language/es/site_lang.php
@@ -0,0 +1,68 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Una herramienta que ahorra tiempo a los desarrolladores. Detecta si el navegador está actualizado y aconseja al usuario que actualice a una nueva versión.';
+$lang['btn_closenav'] = 'Cerrar Navegación';
+$lang['mainNav_home'] = 'INICIO';
+$lang['mainNav_project'] = 'EL PROYECTO';
+$lang['mainNav_how'] = 'CÓMO UTILIZAR';
+$lang['mainNav_faq'] = 'PREGUNTAS FRECUENTES';
+$lang['mainNav_git'] = 'PÁGINA GITHUB';
+
+/*SHARE*/
+$lang['share_share'] = 'Compartir';
+$lang['share_twitter'] = 'Una %23herramienta que ahorra tiempo a los %23desarrolladores. Detecta si el %23navegador está actualizado y aconseja al usuario que actualice a una nueva versión.';
+$lang['share_twitter_t'] = 'Compartir en Twitter';
+$lang['share_fb_t'] = 'Compartir en Facebook';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Cerrar';
+$lang['txt_keepcalm'] = 'Tranquilo  <br><span class="xs">esta es una herramienta para el</span> ordenador de sobremesa<br><span class="xs"> no </ span> dispositivos móviles ';
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Para una mejor experiencia, mantenga su navegador actualizado. Compruebe aquí las versiones más recientes.';
+$lang['home_subtitle_nojs'] = 'El Javascript no está activado en su navegador. Por favor, activarlo para una mejor experiencia.';
+$lang['home_subtitle_mobile'] = 'Esta es una herramienta para el ordenador de sobremesa, <br> pero no dude en explorar.';
+$lang['home_percentage'] = 'Personas que utilizan<br>este navegador';
+$lang['home_download'] = 'DESCARGAR';
+$lang['home_version'] = 'VERSIÓN';
+$lang['home_available'] = 'DISPONIBLE PARA';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Una Causa <br>Desesperante!';
+$lang['proj_desc1'] = 'Después de pasar demasiado tiempo en la optimización de nuestros sitios web para navegadores antiguos, nosotros en Büro, decidimos crear una solución que no sólo nos ahorra tiempo sino que también nos permite ir a casa temprano<br><br> Pizzas no incluidas!';
+$lang['proj_alt1'] = 'desesperada';
+$lang['proj_desc1_2'] = 'Un dev volviéndose loco';
+
+$lang['proj_title2'] = '¿Cómo <br>funciona?';
+$lang['proj_desc2'] = 'Cuando se ejecuta, el script detecta si el navegador soporta una propiedad CSS. Si no lo hace, se mostrará un aviso al utilizador que le aconseja actualizar su navegador.';
+$lang['proj_desc2_2'] = 'Un sitio web que se ejecuta en un navegador viejo';
+
+$lang['proj_title3'] = '3 pasos <br>para empezar <br>a usarlo!';
+$lang['proj_desc3'] = 'Sólo se tarda 3 minutos. Incluye el archivo JS y CSS y pegar unas pocas líneas de HTML. <br><br>Para más detalles, consulte el <a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github">Github</a> page ;)';
+$lang['proj_desc3_2'] = 'desarrollado <br> en Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Descargar<br> los archivos';
+$lang['how_desc1'] = "La carpeta contiene todos los archivos necesarios: JS, CSS y lenguas.";
+$lang['how_desc1_2'] = 'Descarga en Github';
+
+$lang['how_title2'] = 'incluirlos <br> en su proyecto';
+$lang['how_desc2'] = 'Cargue el archivo CSS en el &lt;head&gt; del documento y el archivo JS al final del &lt;body&gt;.';
+$lang['how_desc2_2'] = 'Copiar al portapapeles ';
+$lang['how_alt2'] = 'navegador ';
+
+$lang['how_title3'] = 'Incluye <br>el HTML <br>Necesario';
+$lang['how_desc3'] = 'You can place this code right before the &lt;/body&gt; tag.';
+$lang['how_desc3_2'] = '
+<h6>Usted está usando un navegador obsoleto.</h6>
+<pActualice su navegador para una mejor experiencia visual en este sitio web. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Actualice su navegador ahora</a></p>
+<p class="last"> <a href="#" id="btnCloseUpdateBrowser" title="Close"> &times; </a> </p>
+';
+
+$lang['how_title4'] = 'Llamar al <br>Plugin y';
+$lang['how_alt4'] = 'hecho';
+$lang['how_desc4'] = 'A partir de ahora, cualquier usuario con un navegador obsoleto verá una advertencia en la parte superior de la página.<br><br> Si te gusta este proyecto, compártelo con el mundo!';
+$lang['how_desc4_2'] = '//plugin function, place inside DOM ready function';
+
