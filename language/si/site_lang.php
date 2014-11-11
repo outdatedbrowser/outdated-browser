092406f4d706c1871fa6352f86b3b12e9521aee7
Davor Padovan <davor.padovan@gmail.com>
2014-11-11 11:24:30 +0100

    Slovenian website translation.

---
+++ language/si/site_lang.php
@@ -0,0 +1,72 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Orodje, ki razvijalcem prihrani čas. Zazna zastarele brskalnike in uporabnikom ponudi posodobitev.';
+$lang['btn_closenav'] = 'Zapri navigacijo';
+$lang['mainNav_home'] = 'DOMOV';
+$lang['mainNav_project'] = 'O PROJEKTU';
+$lang['mainNav_how'] = 'UPORABA';
+$lang['mainNav_faq'] = 'POMOČ';
+$lang['mainNav_git'] = 'GITHUB';
+
+/*SHARE*/
+$lang['share_share'] = 'Deli';
+$lang['share_twitter'] = 'Orodje, ki razvijalcem prihrani čas. Zazna zastarele brskalnike in uporabnikom ponudi posodobitev.';
+$lang['share_twitter_t'] = 'Deli na Twitter';
+$lang['share_fb_t'] = 'Deli na Facebook';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Zapri';
+$lang['txt_keepcalm'] = 'brez skrbi<br><span class="xs">to je orodje za</span>računalnike<br><span class="xs">ne za</span>mobilne naprave';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Za boljši izkušnjo imejte brskalnik posodobljen. Tukaj preverite za zadnjo izdajo.';
+$lang['home_subtitle_nojs'] = 'Javascript je izklopljen. Za boljšo izkušnjo ga vklopite.';
+$lang['home_subtitle_mobile'] = 'To je orodje za računalnike, <br>toda raziskovanje je zaželjeno.';
+$lang['home_percentage'] = 'ljudi uporablja<br>ta brskalnik';
+$lang['home_download'] = 'PRENOS';
+$lang['home_version'] = 'VERZIJA';
+$lang['home_available'] = 'NA VOLJO ZA';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Vzrok se skriva<br>v obupu!';
+$lang['proj_desc1'] = 'Ker je optimizacija za zastarele brskalnike zahtevala preveč časa, smo se v agenciji Büro odločili, da naredimo rešitev, ki ne bo samo prihranila časa, temveč nam bo omogoča, da prej odhajamo domov.<br><br>Pizze niso vključene!';
+$lang['proj_alt1'] = 'obup';
+$lang['proj_desc1_2'] = 'Razvijalec, ki se mu meša.';
+
+$lang['proj_title2'] = 'Kako<br>deluje?';
+$lang['proj_desc2'] = 'Naša skripta zazna, ali brskalnik podpira določeno CSS lastnost. Če je ne podpira, prikažemo lepo in jasno opozorilo, ki uporabniku svetuje, naj posodobi svoj brskalnik.';
+$lang['proj_desc2_2'] = 'Spletna stran v zastarelem brskalniku.';
+
+$lang['proj_title3'] = '3 koraki do<br>začetka uporabe!';
+$lang['proj_desc3'] = 'Namestitev traja samo 3 minute. Vključite JS in CSS datoteki, ter kopirajte in prilepite nekaj HTML vrstic.<br><br>Za več informacij preverite našo <a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github">Github</a> stran ;)';
+$lang['proj_desc3_2'] = 'Razvito v<br>Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Prenesite<br>paket';
+$lang['how_desc1'] = "Ta paket vključuje vse zahtevane datoteke: JS, CSS in prevode.";
+$lang['how_desc1_2'] = 'Prenos na Github';
+
+$lang['how_title2'] = 'Vključite jih<br>v svoj projekt';
+$lang['how_desc2'] = 'Vključite CSS v &lt;head&gt; element in JS datoteko na koncu &lt;body&gt;.';
+$lang['how_desc2_2'] = 'Kopirajte kodo v odložišče ';
+$lang['how_alt2'] = 'brskalnik';
+
+$lang['how_title3'] = 'Dodajte še<br>zahtevani<br>HTML';
+$lang['how_desc3'] = 'To kodo dodajte takoj za &lt;body&gt; element.';
+$lang['how_desc3_2'] = '
+<h6>Vaš brskalnik je zastarel!</h6>
+<p>Za pravilen prikaz spletne strani posodobite vaš brskalnik. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Posodobi brskalnik </a></p>
+<p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Zapri">&times;</a></p>
+';
+
+$lang['how_title4'] = 'Vključite<br>vtičnik in';
+$lang['how_alt4'] = 'končano';
+$lang['how_desc4'] = 'Od sedaj bo vsak uporabnik z zastarelim brskalnikom obveščen s prijaznim obvestilom na vrhu strani.<br><br>Če vam je projekt všeč, ga uporabite in delite s svetom!';
+$lang['how_desc4_2'] = '//funkcija vtičnika, vključite jo, ko je DOM pripravljen';
+
+/*404*/
+$lang['url404'] = "http://outdatedbrowser.com/si";
+$lang['txt_keepcalm_error'] = 'brez skrbi<br><span class="xs">napaka 404</span>stran<br><span class="xs">ki jo iščete</span>ne obstaja';
