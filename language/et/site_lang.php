601ced5bb8408a0d38d1364588e51819b373a86b
Klemens Arro <klemensarro@gmail.com>
2014-11-28 15:56:58 +0200

    Added Estonian translation

---
+++ language/et/site_lang.php
@@ -0,0 +1,72 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Arendajatele aega säästev tööriist. See skript tuvastab aegunud veebilehitseja, ning soovitab kasutajal seda uuendada.';
+$lang['btn_closenav'] = 'Sulge Menüü';
+$lang['mainNav_home'] = 'AVALEHT';
+$lang['mainNav_project'] = 'PROJEKTIST';
+$lang['mainNav_how'] = 'KUIDAS KASUTADA';
+$lang['mainNav_faq'] = 'KÜSIMUSED';
+$lang['mainNav_git'] = 'GITHUB LEHT';
+
+/*SHARE*/
+$lang['share_share'] = 'Jaga';
+$lang['share_twitter'] = 'Arendajatele aega säästev tööriist. See skript tuvastab aegunud veebilehitseja, ning soovitab kasutajal seda uuendada.';
+$lang['share_twitter_t'] = 'Jaga Twitteris';
+$lang['share_fb_t'] = 'Jaga Facebookis';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Sulge';
+$lang['txt_keepcalm'] = 'säilita rahu <br><span class="xs">see on tööriist</span> arvutitele<br><span class="xs">mitte</span> mobiilsetele seadmetele';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Selleks, et kogeda veebi kogu ilus ja funktsionaalsuses, hoia oma veebilehitseja uuendatud. Vali siit omale sobiv uusim versioon.';
+$lang['home_subtitle_nojs'] = 'Sinu veebilehitsejas on JavaScript keelatud. Lülita see parema kogemuse saamiseks sisse.';
+$lang['home_subtitle_mobile'] = 'See on tööriist arvutitele, <br>kuid ära pelga siin ringi uurida.';
+$lang['home_percentage'] = 'inimestest kasutab <br>seda veebilehitsejat';
+$lang['home_download'] = 'LAE ALLA';
+$lang['home_version'] = 'VERSIOON';
+$lang['home_available'] = 'SAADAVAL SÜSTEEMIDELE';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Lootusetu <br> olukord!';
+$lang['proj_desc1'] = 'Peale liigset aja põletamist optimeerimaks veebilehti vanadele veebilehitsejatele, otsustasime me Büro agentuuris teha lahenduse, mis mitte ainult ei säästa Su aega, vaid võimaldab Sul ka varem koju saada.<br><br> Pakkumine ei sisalda pitsasid!';
+$lang['proj_alt1'] = 'lootusetu';
+$lang['proj_desc1_2'] = 'Arendaja mõistust kaotamas';
+
+$lang['proj_title2'] = 'Kuidas see <br>töötab?';
+$lang['proj_desc2'] = 'Käivitamisel tuvastab see skript kas veebilehitseja toetab vajalike CSS funktsioone. Kui ei toeta, siis me kuvame ilusat teadet kasutajale, kus me soovitame tal veebilehitsejat uuendada.';
+$lang['proj_desc2_2'] = 'Veebileht mis kasutab aegunud veebilehitsejat';
+
+$lang['proj_title3'] = '3 sammu <br> lahenduseni!';
+$lang['proj_desc3'] = 'See võtab ainult 3 minutit. Lae oma lehele JS ja CSS, ning lisa mõned read HTMLi. <br><br>Rohkema info saamiseks, vaata <a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github">Githubi</a> lehte ;)';
+$lang['proj_desc3_2'] = 'Arendas <br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Lae alla <br> pakett';
+$lang['how_desc1'] = "Meie pakett sisaldab kõiki vajalike faile: JS, CSS ja tõlked.";
+$lang['how_desc1_2'] = 'Lae alla Githubist';
+
+$lang['how_title2'] = 'Lisa need <br> oma projekti';
+$lang['how_desc2'] = 'Lae CSS fail &lt;head&gt; osasse ja JS fail &lt;body&gt; lõppu.';
+$lang['how_desc2_2'] = 'Kopeeri vajalik kood ';
+$lang['how_alt2'] = 'veebilehitseja ';
+
+$lang['how_title3'] = 'Lisa vajalik <br> HTML';
+$lang['how_desc3'] = 'Sa võid selle koodi lisada vahetule enne &lt;/body&gt; osa.';
+$lang['how_desc3_2'] = '
+<h6>Sinu veebilehitseja on vananenud!</h6>
+<p>Palun uuenda oma veebilehitsejat, et näha lehekülge korrektselt. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/et">Uuenda oma veebilehitsejat kohe</a></p>
+<p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p>
+';
+
+$lang['how_title4'] = 'Käivita <br> plugin ja';
+$lang['how_alt4'] = 'valmis';
+$lang['how_desc4'] = 'Nüüdsest kuvatakse kõigile aegunud veebilehitsejaga külastajatele ilusat teadet lehe ülemises osas.<br><br> Kui sulle meeldib see projekt, siis jaga seda maailmaga!';
+$lang['how_desc4_2'] = '//plugina kood, aseta see DOM ready funktsiooni';
+
+/*404*/
+$lang['url404'] = "http://outdatedbrowser.com/et";
+$lang['txt_keepcalm_error'] = 'säilita rahu <br><span class="xs">404 viga</span>Lehekülge <br> mida sa otsid<br><span class="xs">ei suudetud leida</span>';
