2f510ee29d3d7588e1fb93ed60fdf0f61cbf365b
Georgios Pligoropoulos <george@pligor.com>
2015-06-05 11:54:23 +0300

    Create site_lang.php
    This is the (first version of the) Greek translation. I hope everything is ok. Check it out and please let me know when it is online so we could check it again in the website. If there are any apparent mistakes do not hesitate to contact us. Thank you very much

---
+++ language/gr/site_lang.php
@@ -0,0 +1,72 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Ένα εργαλείο εξοικονόμησης χρόνου για τους μηχανικούς πληροφορικής. Ανιχνεύει  προγράμματα περιήγησης/φυλλομετρητές (browser) που είναι ξεπερασμένοι και συνιστά στους χρήστες να αναβαθμίσουν στην νέα έκδοση.';
+$lang['btn_closenav'] = 'Κλείσιμο Πλοήγησης';
+$lang['mainNav_home'] = 'ΑΡΧΙΚΗ ΣΕΛΙΔΑ';
+$lang['mainNav_project'] = 'ΤΟ ΕΡΓΟ';
+$lang['mainNav_how'] = 'ΠΩΣ ΝΑ ΤΟ ΧΡΗΣΙΜΟΠΟΙΗΣΕΙΣ';
+$lang['mainNav_faq'] = 'ΣΥΧΝΕΣ ΕΡΩΤΗΣΕΙΣ';
+$lang['mainNav_git'] = 'GITHUB ΣΕΛΙΔΑ';
+
+/*SHARE*/
+$lang['share_share'] = 'Κάνε Share';
+$lang['share_twitter'] = 'Εργαλείο που σώζει πολύτιμο χρόνο σε προγραμματιστές.Ανιχνεύει ξεπερασμένους browsers και συνιστά στους χρήστες την αναβάθμιση σε νέα έκδοση';
+$lang['share_twitter_t'] = 'Κάνε share στο Twitter';
+$lang['share_fb_t'] = 'Κάνε share στο Facebook';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Κλείσιμο';
+$lang['txt_keepcalm'] = 'μείνε ήρεμος <br><span class="xs">αυτό είναι ένα εργαλείο για το</span> desktop<br><span class="xs">όχι</span> για φορητές συσκευές';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Για μια καλύτερη εμπειρία, διατήρησε το πρόγραμμα περιήγησης/φυλλομετρητή (browser) σύγχρονο και φρέσκο. Δες εδώ τις τελευταίες εκδόσεις.';
+$lang['home_subtitle_nojs'] = 'Το javascript είναι απενεργοποιημένο στον browser που χρησιμοποιείς αυτή τη στιγμή. Ενεργοποίησέ το για μια καλύτερη εμπειρία.';
+$lang['home_subtitle_mobile'] = 'Αυτό είναι ένα εργαλείο για το desktop, <br>αλλά είσαι ελεύθερος να το εξερευνήσεις.';
+$lang['home_percentage'] = 'άνθρωποι που χρησιμοποιούν <br>αυτό το πρόγραμμα περιήγησης';
+$lang['home_download'] = 'ΚΑΤΕΒΑΣΕ (DOWNLOAD)';
+$lang['home_version'] = 'ΕΚΔΟΣΗ';
+$lang['home_available'] = 'ΔΙΑΘΕΣΙΜΟ ΓΙΑ';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Ένα Aπελπισμένο <br> Κίνημα!';
+$lang['proj_desc1'] = 'Αφού περάσαμε πολύ χρόνο σε βελτιστοποίηση ιστοσελίδων για παλαιότερα προγράμματα περιήγησης, παλιούς φυλλομετρητές, εμείς στο Büro, αποφασίσαμε να δημιουργήσουμε μια λύση που όχι μόνο εξοικονομεί χρόνο, αλλά κυριολεκτικά σε στέλνει στο σπίτι νωρίτερα.<br><br> Δεν περιλαμβάνονται πίτσες!';
+$lang['proj_alt1'] = 'απελπισία';
+$lang['proj_desc1_2'] = 'Ένας μηχανικός πληροφορικής τρελάθηκε';
+
+$lang['proj_title2'] = 'Πως<br>Λειτουργεί?';
+$lang['proj_desc2'] = 'Όταν εκτελείται, αυτό το script ανιχνεύει αν το πρόγραμμα περιήγησης υποστηρίζει κάποια CSS ιδιότητα. Αν δεν υποστηρίζει θα εμφανιστεί μια ωραία και cool προειδοποίηση που θα συμβουλεύει το χρήστη να ενημερώσει το πρόγραμμα περιήγησης.';
+$lang['proj_desc2_2'] = 'Μια ιστοσελίδα που εκτελείται σε ένα παλιό πρόγραμμα περιήγησης';
+
+$lang['proj_title3'] = '3 βήματα για να <br> αρχίσεις να το χρησιμοποιείς!';
+$lang['proj_desc3'] = 'Χρειάζονται μόνο 3 λεπτά. Κάνε include το JS και το CSS αρχείο και μετά κάνε paste δύο γραμμές HTML.<br><br>Για περισσότερες πληροφορίες τσέκαρε την <a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github">Github</a> σελίδα ;)';
+$lang['proj_desc3_2'] = 'Η ανάπτυξη έγινε στα <br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Κατέβασε <br> το Πακέτο';
+$lang['how_desc1'] = "Το πακέτο μας περιλαμβάνει όλα τα απαιτούμενα αρχεία: JS, CSS και ξένες γλώσσες.";
+$lang['how_desc1_2'] = 'Κατέβασε από Github';
+
+$lang['how_title2'] = 'Κάντα Include <br> στο Project σου';
+$lang['how_desc2'] = 'Φορτώσε το CSS αρχείο στο &lt;head&gt; και το js αρχείο στο τέλος του &lt;body&gt;.';
+$lang['how_desc2_2'] = 'Κάνε copy αυτόν τον κώδικα στο clipboard ';
+$lang['how_alt2'] = 'πρόγραμμα περιήγησης / φυλλομετρητής ';
+
+$lang['how_title3'] = 'Κάνε Include την <br> Απαιτούμενη <br> HTML';
+$lang['how_desc3'] = 'Μπορείς να τοποθετήσεις αυτόν τον κώδικα ακριβώς πριν από την ετικέτα &lt;/body&gt;.';
+$lang['how_desc3_2'] = '
+<h6>Το προγραμμα περιηγησης (φυλλομετρητης/browser) που χρησιμοποιεις για το internet δεν ειναι ενημερωμενο!</h6>
+<p>Ενημερωσε το προγραμμα περιηγησης για να δεις αυτη την ιστοσελιδα σωστα.<a target="_blank" id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Ενημερωση του προγραμματος περιηγησης τωρα</a></p>
+<p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p>
+';
+
+$lang['how_title4'] = 'Κάλεσε το <br> Plugin και';
+$lang['how_alt4'] = 'ολοκλήρωση';
+$lang['how_desc4'] = 'Από εδώ και στο εξής, οποιοσδήποτε χρήστης με ένα παλιό πρόγραμμα περιήγησης (παλιό φυλλομετρητή/browser) θα βλέπει μια φανταχτερή προειδοποίηση στην κορυφή της σελίδας. <br><br>Αν σου αρέσει αυτό το project, κάντο fork και μοιράσου το με τον κόσμο!';
+$lang['how_desc4_2'] = '//plugin function, τοποθέτησε το μέσα σε μία DOM ready function';
+
+/*404*/
+$lang['url404'] = "http://outdatedbrowser.com/gr";
+$lang['txt_keepcalm_error'] = 'Ηρέμησε δεν υπάρχει λόγος πανικού <br><span class="XS">σφάλμα 404</ span>Η σελίδα που<br>ψάχνεις<br><span class="XS">δεν μπορεί να βρεθεί</ span>';
