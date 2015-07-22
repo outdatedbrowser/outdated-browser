92ffa4c791891645b2065c80a41ea5c34092735a
samsam2310 <samsam2310@gmail.com>
2015-07-22 22:04:27 +0800

    add Traditional Chinese

---
+++ language/tw/site_lang.php
@@ -0,0 +1,69 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = '一個為開發者節省時間的工具。它可以檢測過時的瀏覽器並建議用戶將其升級至新版本。 ';
+$lang['btn_closenav'] = '關閉導航';
+$lang['mainNav_home'] = '首頁';
+$lang['mainNav_project'] = '專案';
+$lang['mainNav_how'] = '如何使用';
+$lang['mainNav_faq'] = '常見問題';
+$lang['mainNav_git'] = 'GitHub 頁面';
+
+/*SHARE*/
+$lang['share_share'] = '分享';
+$lang['share_twitter'] = '為#開發者節省時間的#工具。它可以檢測#過時的#瀏覽器並建議用戶將其#升級至新版本。 ';
+$lang['share_twitter_t'] = '分享到 Twitter';
+$lang['share_fb_t'] = '分享到 Facebook';
+
+
+/*GENERIC*/
+$lang['txt_close'] = '關閉';
+$lang['txt_keepcalm'] = '冷靜<br><span class="xs">這是為</span>桌面<br>用戶提供的<span class="xs">不是為</span>手機用戶提供的。 ';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = '將您的瀏覽器保持在最新版本，以獲得最佳的瀏覽體驗。在這裡您可以獲得最新的版本。 ';
+$lang['home_subtitle_nojs'] = '您的瀏覽器已禁用Javascript，啟用它​​獲得更好的體驗。 ';
+$lang['home_subtitle_mobile'] = '這是為桌面用戶準備的工具，<br>但您可以安心在這裡瀏覽';
+$lang['home_percentage'] = '的人們在使用<br>這款瀏覽器';
+$lang['home_download'] = '下載';
+$lang['home_version'] = '版本';
+$lang['home_available'] = '在以下操作系統可用';
+
+/*PROJECT*/
+$lang['proj_title1'] = '絕望的 <br> 原因!';
+$lang['proj_desc1'] = '在為舊版瀏覽器優化我們的網站花費太多時間之後，我們決定在Büro創建一個, 不僅節約您的時間，而且讓您儘早回家休息的解決方案。 <br><br> Pizzas not included!';
+$lang['proj_alt1'] = '絕望';
+$lang['proj_desc1_2'] = '抓狂的開發者';
+
+$lang['proj_title2'] = '它如何 <br> 運作？ ';
+$lang['proj_desc2'] = '在程式碼執行後，檢測瀏覽器是否支援指定的CSS 屬性。如果不支援，我們將顯示一個美觀的警告建議用戶更新瀏覽器。 ';
+$lang['proj_desc2_2'] = '在舊版本瀏覽器上運行的網站';
+
+$lang['proj_title3'] = '只需3 步<br> 即可使用！ ';
+$lang['proj_desc3'] = '完成這些只需3 分鐘。包含JS 和CSS 文件，在HTML 中貼上幾行程式碼。 <br><br>想知道更多資訊，請在<a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github" >Github</a> 頁面查看;)';
+$lang['proj_desc3_2'] = '開發於<br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = '下載 <br> 套件包';
+$lang['how_desc1'] = "我們的套件包包含所有需要使用的檔案：JS, CSS 和語言檔案。";
+$lang['how_desc1_2'] = '從 Github 下載';
+
+$lang['how_title2'] = '在您的項目中<br> 引入它們';
+$lang['how_desc2'] = '在&lt;head&gt; 元素中引入CSS，在&lt;body&gt; 最後引入JS。 ';
+$lang['how_desc2_2'] = '複製程式碼至剪貼簿';
+$lang['how_alt2'] = '瀏覽器 ';
+
+$lang['how_title3'] = '加入<br> 需要的<br> HTML 代碼';
+$lang['how_desc3'] = '您需要在&lt;/body&gt; 標籤開頭放置此代碼。 ';
+$lang['how_desc3_2'] = '
+<h6>您的瀏覽器已過時</h6>
+<p>要正常瀏覽本網站請升級您的瀏覽器。 <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">現在升級​​</a></p>
+<p class="last"> <a href="#" id="btnCloseUpdateBrowser" title="關閉"> &times; </a> </p>
+';
+
+$lang['how_title4'] = '調用 <br> 插件即';
+$lang['how_alt4'] = '完成';
+$lang['how_desc4'] = '從現在開始，所有使用舊版瀏覽器的用戶都將在頁面頂端看到漂亮的提示。 <br><br>如果您喜歡本專案，請Fork 它然後在全世界分享它！ ';
+$lang['how_desc4_2'] = '//插件函式，將其放入DOM 加載完畢後的函式中';
+
