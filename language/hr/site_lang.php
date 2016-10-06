6992b22622f1b6bbbfce676d80f44aa454d0c15f
thebasix <livemsn22@gmail.com>
2016-10-06 17:40:58 +0200

    Croatian Language

---
+++ language/hr/site_lang.php
@@ -0,0 +1,72 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Alat za programere koji štedi vrijeme. Detektira stare verzije Internet preglednika i savjetuje korisnike da isti ažuriraju.';
+$lang['btn_closenav'] = 'Sakrij navigaciju';
+$lang['mainNav_home'] = 'POČETNA';
+$lang['mainNav_project'] = 'O PROJEKTU';
+$lang['mainNav_how'] = 'KAKO KORISTITI';
+$lang['mainNav_faq'] = 'ČESTA PITANJA';
+$lang['mainNav_git'] = 'GITHUB STRANICA';
+
+/*SHARE*/
+$lang['share_share'] = 'Podijeli';
+$lang['share_twitter'] = '%23Alat za %programere koji štedi vrijeme. Detektira %23stare %23verzije %23Internet %23preglednika i savjetuje korisnike da isti %23ažuriraju';
+$lang['share_twitter_t'] = 'Podijeli na Twitteru';
+$lang['share_fb_t'] = 'Podijeli na Facebooku';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Zatvori';
+$lang['txt_keepcalm'] = 'keep calm <br><span class="xs">ovo je alat za</span>desktop<br><span class="xs">a ne</span> mobilne uređaje';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Za bolje korisničko iskustvo, ažurirajte Internet preglednik. Provjerite ovdje za najnoviju verziju.';
+$lang['home_subtitle_nojs'] = 'Javascript je onemogućen u Vašem Internet pregledniku. Omogućite Javascript za bolje korisničko iskustvo.';
+$lang['home_subtitle_mobile'] = 'Ovo je alat za desktop uređaje, <br>no slobodno istražite.';
+$lang['home_percentage'] = 'ljudi koristi <br>ovaj preglednik';
+$lang['home_download'] = 'PREUZMI';
+$lang['home_version'] = 'VERZIJA';
+$lang['home_available'] = 'DOSTUPAN ZA';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Rezultat <br> Očajništva!';
+$lang['proj_desc1'] = 'Nakon mnogo potrošenog vremena na optimizaciju stranica za starije Internet preglednik, mi u firmi Büro, odlučili smo napraviti alat koji ne samo da štedi Vaše vrijeme, već Vam omogućuje raniji odlazak s posla.';
+$lang['proj_alt1'] = 'očajan';
+$lang['proj_desc1_2'] = 'Očajni programer';
+
+$lang['proj_title2'] = 'Kako <br> Radi?';
+$lang['proj_desc2'] = 'Prilikom izvršavanja, skripta detektira podržava li Internet preglednik CSS svojstvo ili ne. Ukoliko ne podržava, na ekranu će biti prikazano malo i cool upozorenje za korisnika da ažurira svoj preglednik.';
+$lang['proj_desc2_2'] = 'Stranica u starom pregledniku';
+
+$lang['proj_title3'] = '3 Koraka <br> Do Početka Korištenja!';
+$lang['proj_desc3'] = 'Potrebne su Vam 3 minute. Dodajte u svoj kod JS i CSS datoteke, kao i par linija HTML koda.<br><br>Za više detalja posjetite <a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github">Github</a> stranicu ;)';
+$lang['proj_desc3_2'] = 'Nastalo u <br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Preuzmi <br> dodatak';
+$lang['how_desc1'] = "Naš dodatak sadrži sve potrebne datoteke: JS, CSS i jezike.";
+$lang['how_desc1_2'] = 'Preuzmi sa Github-a';
+
+$lang['how_title2'] = 'Dodajte ih <br> u Vaš Projekt';
+$lang['how_desc2'] = 'Uključite css datoteku u &lt;head&gt; element i JS datoteku na kraju &lt;body&gt; elementa.';
+$lang['how_desc2_2'] = 'Kopiraj ovaj kod ';
+$lang['how_alt2'] = 'preglednik ';
+
+$lang['how_title3'] = 'Dodajte <br> Obvezni <br> HTML';
+$lang['how_desc3'] = 'Ovaj kod stavite odmah prije &lt;/body&gt; tag-a.';
+$lang['how_desc3_2'] = '
+<h6>Vaš Internet preglednik nije ažuriran!</h6>
+<p>Ažurirajte Vaš preglednik za ispravan prikaz ove stranice. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Ažuriraj moj preglednik</a></p>
+<p class="last"> <a href="#" id="btnCloseUpdateBrowser" title="Zatvori"> &times; </a> </p>
+';
+
+$lang['how_title4'] = 'Uključite <br> Dodatak i';
+$lang['how_alt4'] = 'gotovo';
+$lang['how_desc4'] = 'Odsad pa nadalje, svakom korisniku sa starim Internet preglednikom biti će prikazana fensi obavijest na početku starnice.<br><br> Ukoliko Vam se sviđa ovaj projekt, forkajte ga i share-ajte sa svijetom!';
+$lang['how_desc4_2'] = '//inicijalizacijska funkcija, stavite unutar DOM ready funkcije';
+
+/*404*/
+$lang['url404'] = "http://outdatedbrowser.com/en";
+$lang['txt_keepcalm_error'] = 'keep calm <br><span class="xs">404 error</span>Stranica <br> koju tražite<br><span class="xs">nije pronađena</span>';
