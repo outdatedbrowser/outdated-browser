5071880d711a2588f17dd7df043bce015933a9f7
Marek Suscak <marek.suscak@gmail.com>
2016-10-11 01:49:21 +0200

    Add website slovak translation.

---
+++ language/sk/site_lang.php
@@ -0,0 +1,72 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'Nástroj šetriaci čas vývojárom. Detekuje zastarané prehliadače a radí používateľom, aby prešli na novú verziu.';
+$lang['btn_closenav'] = 'Zavrieť navigáciu';
+$lang['mainNav_home'] = 'HLAVNÁ STRÁNKA';
+$lang['mainNav_project'] = 'O PROJEKTE';
+$lang['mainNav_how'] = 'AKO POUŽÍVAŤ';
+$lang['mainNav_faq'] = 'ČASTÉ OTÁZKY';
+$lang['mainNav_git'] = 'GITHUB';
+
+/*SHARE*/
+$lang['share_share'] = 'Zdieľať';
+$lang['share_twitter'] = '%23Nástroj šetriaci čas %23vývojárom. Detekuje %23zastarané %23prehliadače a radí používateľom, aby prešli na novú verziu.';
+$lang['share_twitter_t'] = 'Zdieľať na Twitteri';
+$lang['share_fb_t'] = 'Zdieľať na Facebooku';
+
+
+/*GENERIC*/
+$lang['txt_close'] = 'Zavrieť';
+$lang['txt_keepcalm'] = 'Nepanikárte <br><span class="xs">tento nástroj je určený pre</span>desktop<br><span class="xs">nie pre</span> mobilné zariadenia';
+
+
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'Pre lepší zážitok z prehliadania webstránok udržiavajte svoj prehliadač vždy aktuálny. Skontrolujte, či máte najnovšiu verziu.';
+$lang['home_subtitle_nojs'] = 'Javascript je zakázaný v nastaveniach vášho prehliadača. Pre správne zobrazenie ho povoľte.';
+$lang['home_subtitle_mobile'] = 'Tento nástroj je určený pre desktop, <br>ale kľudne si ho vyskúšajte.';
+$lang['home_percentage'] = 'ľudí používa <br>tento prehliadač';
+$lang['home_download'] = 'STIAHNÚŤ';
+$lang['home_version'] = 'VERZIA';
+$lang['home_available'] = 'DOSTUPNÝ PRE';
+
+/*PROJECT*/
+$lang['proj_title1'] = 'Príčina <br> zúfalstva!';
+$lang['proj_desc1'] = 'Po tom, čo sme strávili príliš veľa času optimalizovaním webstránok pre staré prehliadače, sme sa v Büro rozhodli vytvoriť riešenie, ktoré nie len šetrí váš čas, ale takisto vás pošle domov skôr.<br><br> Pizza nie je súčasťou dodávky!';
+$lang['proj_alt1'] = 'príčina zúfalstva';
+$lang['proj_desc1_2'] = 'Vývojári šalejú';
+
+$lang['proj_title2'] = 'Ako to<br>funguje?';
+$lang['proj_desc2'] = 'Po spustení, tento skript skontroluje, či prehliadač podporuje určité CSS vlastnosti. Ak nie, zobrazíme používateľovi pekné upozornenie, aby aktualizoval prehliadač.';
+$lang['proj_desc2_2'] = 'Webstránka zobrazujúca sa v zastaranom prehliadači';
+
+$lang['proj_title3'] = '3 kroky k <br> používaniu!';
+$lang['proj_desc3'] = 'Trvá to len 3 minúty. Stačí zahrnúť JS a CSS súbor, a vložiť zopár riadkov HTML. <br><br>Pre získanie viac informácií navštívte stránku <a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github">Githubu</a> ;)';
+$lang['proj_desc3_2'] = 'Vytvorené v <br> Bürolabs';
+
+/*HOW*/
+$lang['how_title1'] = 'Stiahnite <br> balíček';
+$lang['how_desc1'] = "Náš balíček obsahuje všetky potrebné súbory: JS, CSS a jazykové mutácie.";
+$lang['how_desc1_2'] = 'Stiahnúť na GitHube';
+
+$lang['how_title2'] = 'Pridajte ich <br> do vášho projektu';
+$lang['how_desc2'] = 'Pridajte CSS súbor do &lt;head&gt; elementu a JS súbor na koniec &lt;body&gt;.';
+$lang['how_desc2_2'] = 'Skopírovať tento kód ';
+$lang['how_alt2'] = 'prehliadač ';
+
+$lang['how_title3'] = 'Vložte <br> vyžadované <br> HTML';
+$lang['how_desc3'] = 'Tento kód môžete vložiť hneď pred &lt;/body&gt; tag.';
+$lang['how_desc3_2'] = '
+<h6>Váš prehliadač je zastaraný!</h6>
+<p>Pre správne zobrazenie tejto webstránky aktualizujte svoj prehliadač. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/sk">Aktualizovať prehliadač teraz</a></p>
+<p class="last"> <a href="#" id="btnCloseUpdateBrowser" title="Zavrieť"> &times; </a> </p>
+';
+
+$lang['how_title4'] = 'Spustite <br> plugin a';
+$lang['how_alt4'] = 'hotovo';
+$lang['how_desc4'] = 'Od tejto chvíle bude každý používateľ so starým prehliadačom upozornený efektným oznámením v hornej časti webstránky.<br><br> Ak sa vám tento projekt páči, forkujte a zdieľajte s celým svetom!';
+$lang['how_desc4_2'] = '//umiestnite do DOM ready funkcie';
+
+/*404*/
+$lang['url404'] = "http://outdatedbrowser.com/sk";
+$lang['txt_keepcalm_error'] = 'Nepanikárte <br><span class="xs">chyba 404</span>Stránka <br> ktorú hľadáte<br><span class="xs">nebola nájdená</span>';
